0bd14d2d2518a22217768f08737a9a781726583d
onsAouyann <ons.aouyan@eniso.u-sousse.tn>
2025-01-14 21:12:19 +0100

    kurento one2one video call

---
+++ src/main/java/com/dist/interview/kurentobackend/KurentoBackendApplication.java
@@ -0,0 +1,46 @@
+package com.dist.interview.kurentobackend;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+@SpringBootApplication
+@EnableWebSocket
+public class KurentoBackendApplication implements WebSocketConfigurer {
+
+    @Bean
+    public CallHandler callHandler() {
+        return new CallHandler();
+    }
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
+    }
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(32768);
+        return container;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(callHandler(), "/call");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(KurentoBackendApplication .class, args);
+    }
+}
